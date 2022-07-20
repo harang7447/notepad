0bdadcbcbaf0255eb8aee10d7ec1f6cd67d29134
harang7447 <leesky2004@gmail.com>
2022-07-20 12:27:02 +0700

    save memo

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const save = document.querySelector("#save")
 const writememo = document.querySelector("#write")
+const memos = Array()
+
 function savememo(){
-    const memo = writememo.value
-    console.log(memo)
+    memos.push(writememo.value)
+    let div = document.createElement("div")
+    div.innerHTML = memos.slice(-1)[0]
+    memolist.append(div)
+
+    // let newMemo = document.createElement("span")
+    // newMemo.id = `memo${memos.length}`
+    // newMemo.innerHTML = memos.slice(-1)[0]
+    // memos.length.append(newMemo)
+    
 }
 save.addEventListener("click", savememo)
+
+
 // 메모를 배열에 저장, div태그안에 목록추가
