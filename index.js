a871ce4235c14217fadae161777c36e0f8b0cd43
harang7447 <leesky2004@gmail.com>
2022-08-24 13:02:10 +0700

    fix warning message bug

--- index.js
+++ index.js
@@ -57,20 +57,15 @@ function saveAndModMemo(memo){
 }
 
 function warningMessage(){
-    warning.innerHTML = `
-    <div class="alert alert-info alert-dismissible" role="alert" id="liveAlert">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
-            <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-        </svg>
-        매모를 입력하지 않았습니다
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
-    `
+    warning.style.display = "block"
 }
 
 function deleteMemo(deleteButtonId){
-    const targetDate = deleteButtonId.replace("del", "")
-    memos = memos.filter(memo => memo.date !== targetDate)
+    const isDelete = confirm("메모를 삭제하시겟습니까")
+    if(isDelete){
+        const targetDate = deleteButtonId.replace("del", "")
+        memos = memos.filter(memo => memo.date !== targetDate)
+    }
 }
 
 function saveMemosToLocalstorage(){
@@ -86,8 +81,8 @@ function renderMemos(){
     const memoDivs = memos.map(memo =>`
     <div id="memo${memo.date}">
         <p id="val${memo.date}">${memo.text}</p>
-        <button type="button" class="btn btn-primary mb-3 del" id="del${memo.date}" class="del">삭제</button>
-        <button type="button" class="btn btn-light mb-3 mod" id="mod${memo.date}">수정</button>
+        <button type="button" class="btn btn-outline-primary mb-3 del" id="del${memo.date}" class="del">삭제</button>
+        <button type="button" class="btn btn-outline-secondary mb-3 mod" id="mod${memo.date}">수정</button>
     </div>`)
     memoList.innerHTML = memoDivs.join("")
 
@@ -109,12 +104,14 @@ const saveMemo = document.querySelector("#save")
 const writeMemo = document.querySelector("#write")
 const memoList = document.querySelector("#memolist")
 const warning = document.querySelector("#warning")
+const off = document.querySelector("#off")
 
 let memos = loadMemosFromLocalstorage()
 let isAdd = true;
 let modifyingDate = undefined;
 
 saveMemo.addEventListener("click", () => {saveAndModMemo(); renderMemos();})
+off.addEventListener("click", () => {warning.style.display = "none"})
 renderMemos()
 // 1. 수정 버튼을 누르면 textarea #write에 memo.text가 입력될거에요 
 // 2. text area에 수정할 메모를 입력하고 
